2ffee71fd6fc5625f66942a0fa7be14e6bc9fd33
rubensmneto96 <73607804+rubensmneto96@users.noreply.github.com>
2021-03-26 16:19:17 -0300

    Adicionei os arquivos

---
+++ script.js
@@ -0,0 +1,39 @@
+function carregar(){
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('imagem');
+    var horario = document.getElementById('hora');
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var segundo = data.getSeconds();
+
+    horario.innerHTML = `${hora}:${minuto}:${segundo}`;
+
+    if (hora >= 0 && hora < 12) {
+        msg.innerHTML = "Bom dia!";
+        img.src = "manha.jpg";
+        document.body.style.background = "#3399ff";
+    }else if (hora >= 12 && hora < 18) {
+        msg.innerHTML = "Boa tarde!";
+        img.src = "tarde.jpg";
+        document.body.style.background = "#cc6600";
+    } else {
+        msg.innerHTML = "Boa noite!";
+        img.src = "noite.jpg";
+        document.body.style.background = "#333399";
+
+    }
+
+    if (hora < 10) {
+        hora = `0${horas}`;
+    }
+    if (minuto < 10) {
+        minuto = `0${minuto}`;
+    }
+    if (segundo < 10) {
+        segundo =`0${segundo}`;
+    }
+
+}
+
+var mudancaHorario = setInterval(carregar, 1000);
