5fee8a62016ee47a00ae670d19daea6b6661e46c
Rubens <rubensmn96@gmail.com>
2021-12-23 11:28:17 -0300

    Adicionado Bootstrap no projeto

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ function carregar(){
     if (hora >= 0 && hora < 12) {
         msg.innerHTML = "Bom dia!";
         img.src = "manha.jpg";
-        document.body.style.background = "#3399ff";
+        document.body.style.background = "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(147,228,255,1) 0%, rgba(20,153,242,1) 100%)";
     }else if (hora >= 12 && hora < 18) {
         msg.innerHTML = "Boa tarde!";
         img.src = "tarde.jpg";
-        document.body.style.background = "#cc6600";
+        document.body.style.background = "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(249,193,137,1) 0%, rgba(235,135,36,1) 100%)";
     } else {
         msg.innerHTML = "Boa noite!";
         img.src = "noite.jpg";
-        document.body.style.background = "#333399";
+        document.body.style.background = "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(51,51,153,1) 0%, rgba(29,29,101,1) 100%)";
 
     }
 
